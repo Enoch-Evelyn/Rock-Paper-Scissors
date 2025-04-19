2607d768e27c47d58efcb38dc255c042a228f7aa
Knock <enoch10evelyn@gmail.com>
2025-04-18 23:24:04 -0400

    User's input is being recorded and displayed

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-# import random 
+import random 
 
+# This input is where the user chooses what weapon to use
 user_action = input("Enter Choice( rock, paper, scissor):")
 
+computer_possible_actions = ("rock, paper, scissor")
+
+computer_action = random.choice(computer_possible_actions )
+
 print(user_action)
+
+print(computer_possible_actions)
