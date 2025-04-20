38328c538bb5262e7caa10e19969c03a435e9f16
Knock <enoch10evelyn@gmail.com>
2025-04-20 08:48:19 -0400

    Updated print function

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ user_action = input("Enter Choice( rock, paper, scissor):")
 # Possible choices for the computer to choose formatted in an array 
 computer_possible_actions = ["rock", "paper", "scissor"]
 
+#Computer chooses a random element of the computer_possible_actions array  
 computer_action = random.choice(computer_possible_actions )
 
-print(user_action)
 
-print(computer_action)
+#Recorded input from and output from computer formated into a literal string and presented on screen 
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+
