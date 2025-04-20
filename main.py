47d04db16a11a7cd4a0751a840e0c518be453947
Knock <enoch10evelyn@gmail.com>
2025-04-20 08:41:07 -0400

    Computer responds with an action from rock paper scissiors array

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import random
 # This input is where the user chooses what weapon to use
 user_action = input("Enter Choice( rock, paper, scissor):")
 
-computer_possible_actions = ("rock, paper, scissor")
+# Possible choices for the computer to choose formatted in an array 
+computer_possible_actions = ["rock", "paper", "scissor"]
 
 computer_action = random.choice(computer_possible_actions )
 
 print(user_action)
 
-print(computer_possible_actions)
+print(computer_action)
